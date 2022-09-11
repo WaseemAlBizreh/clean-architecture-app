fd0fc4578f0dc61359362679c987079d927c87cf
Waseem AlBizreh <waseem.bez@gmail.com>
2022-09-11 21:03:24 +0300

    first commit

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xffED9728);
+  static const Color gray = Color(0xff737477);
+  static const Color darkGray = Color(0xff525252);
+  static const Color lightGray = Color(0xff9E9E9E);
+}
